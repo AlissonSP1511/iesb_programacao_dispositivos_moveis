de06221d49ae38947fd9c1ca75188b354f032299
Alisson da Silva Pereira <alisson.silva.pereira@gmail.com>
2023-08-22 22:12:33 -0300

    1

--- my-app/App.js
+++ my-app/App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MeuComponente from './componentes/MeuComponente';
+import MinMax 
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <View styles={styles.container}>
+      <MeuComponente/>
+     </View>
   );
 }
 
---
+++ my-app/componentes/MeuComponente.js
@@ -0,0 +1,13 @@
+import { View , Text } from "react-native"
+
+export default function MeuComponente() {
+
+    return(
+    <View>
+        <Text> 
+            Meu Componente
+        </Text>
+    </View>
+
+    ) 
+}
