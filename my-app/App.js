6a85fe2f36d830657580824b54ce56d5055f9b8f
Alisson da Silva Pereira <alisson.silva.pereira@gmail.com>
2023-08-24 13:02:36 -0300

    Número Aleatório

--- my-app/App.js
+++ my-app/App.js
@@ -1,15 +1,17 @@
 
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MeuComponente from './componentes/MeuComponente';
-import MinMax from './componentes/MinMax';
+import MeuComponente from './componentes/Semana01/MeuComponente';
+import MinMax from './componentes/Semana01/MinMax';
+import NumeroAleatorio from './componentes/Semana02/ExercicioNumeroAleatório';
 
 
 
 export default function App() {
   return (
      <View style={styles.container}>
-      <MinMax Min ='20' Max ='40' />
+      {/* <MinMax Min = {20} Max = {40} /> */}
+      <NumeroAleatorio min={1} max={100} />
      </View>
   )
 }
