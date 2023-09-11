80516a92970eb323b8e758c853c59013c7d29a47
Alisson da Silva Pereira <alisson.silva.pereira@gmail.com>
2023-09-11 19:39:25 -0300

    atividade Profile

--- my-app/App.js
+++ my-app/App.js
@@ -1,38 +1,72 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Biscoito from './componentes/Biscoito';
-import Profile from './componentes/Semana04/Profile';
-// import MeuComponente from './componentes/semana02/MeuComponente';
-// import MeuComponente2 from './componentes/semana02/MeuComponente2';
-// import MeuComponente3 from './componentes/semana02/MeuComponente3';
-// import NumeroAleatorio from './componentes/Semana03/NumeroAleatorio';
-// import Botao from './componentes/Semana03/botao';
-// import Contador from './componentes/Semana03/Contador';
-// import DigiteSeuNome from './componentes/Semana03/DigiteSeuNome';
-// import Pai from './componentes/Semana04/ComunicacaoDireta/Pai';
 
+import MeuComponente from './componentes/Semana01/MeuComponente';
+import MinMax from './componentes/Semana01/MinMax';
+
+// import MeuComponente from './componentes/Semana02/MeuComponente';
+import MeuComponente2 from './componentes/Semana02/MeuComponente2';
+import MeuComponente3 from './componentes/Semana02/MeuComponente3';
+
+import Familia from './componentes/Semana03/children/Familia';
+import Filho from './componentes/Semana03/children/Filho';
+
+import Pai from './componentes/Semana03/cominicacao-direta/Pai';
+// import Filho from './componentes/Semana03/cominicacao-direta/Filho';
+
+// import Filho from './componentes/Semana03/comunicacao-indireta/Filho';
+// import Pai from './componentes/Semana03/comunicacao-indireta/Pai';
+
+import Botao from './componentes/Semana03/Botao';
+import ComponenteControlado from './componentes/Semana03/ComponenteControlado';
+import Contador from './componentes/Semana03/Contador';
+import DigiteSeuNome from './componentes/Semana03/DigiteSeuNome';
+import ExercicioNumeroAleatório from './componentes/Semana03/ExercicioNumeroAleatório';
+import NumeroAleatorio from './componentes/Semana03/NumeroAleatorio';
+
+import IntroducaoEstilos from './componentes/Semana04/IntroducaoEstilos';
+import ParOuImpar from './componentes/Semana04/ParOuImpar';
+
+import Biscoito from './componentes/Biscoito';
+import Profile from './componentes/Profile';
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <MeuComponente /> */}
 
-      {/* <MinMax min="10" max="20" /> */}
+      {/* <MeuComponente2 /> */}
 
-      {/* <MeuComponente /> */}
+      {/* <MeuComponente3 /> */}
 
       {/* <NumeroAleatorio min={1} max={60} /> */}
 
       {/* <Botao /> */}
 
+      {/* <ComponenteControlado /> */}
+
       {/* <Contador /> */}
 
-      {/* <DigiteSeuNome/> */}
+      {/* <DigiteSeuNome /> */}
 
-      {/* <Pai/> */}
+      {/* <ExercicioNumeroAleatório min={1} max={60}/> */}
+
+      {/* <Familia /> */}
+
+      {/* <Pai /> */}
+
+      {/* <Filho /> */}
+
+      {/* <MinMax min="10" max="20" /> */}
+
+      {/* <NumeroAleatorio min={1} max={60} /> */}
 
       {/* <Biscoito /> */}
 
       <Profile />
+
+      {/* <ParOuImpar /> */}
+
+      {/* <IntroducaoEstilos /> */}
 
     </View>
   );
